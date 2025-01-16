760b6f628fd08c69e6677724683878da338e52be
Manas <manaschougule2019@gmail.com>
2025-01-16 12:08:11 +0530

    write redis service to add data to cache, get data from cache etc.

---
+++ src/main/java/com/minorproject/library/e_Library/service/RedisService.java
@@ -0,0 +1,35 @@
+package com.minorproject.library.e_Library.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    public RedisService(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void addToCache(String key, String value){
+        this.redisTemplate.opsForValue().set(key,value,10, TimeUnit.MINUTES);
+    }
+
+    public void addToCache(String key, Object value){
+        this.redisTemplate.opsForValue().set(key,value,10,  TimeUnit.MINUTES);
+    }
+
+    public Object getCacheData(String key){
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteFromCache(String key) {
+        this.redisTemplate.delete(key);
+    }
+
+}
