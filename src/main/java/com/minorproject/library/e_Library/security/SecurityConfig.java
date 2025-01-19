3eb8d0a6ead5146f0e6897f46113873255040182
Manas <manaschougule2019@gmail.com>
2025-01-19 20:14:11 +0530

    jwt authentication with rbac

--- src/main/java/com/minorproject/library/e_Library/security/SecurityConfig.java
+++ src/main/java/com/minorproject/library/e_Library/security/SecurityConfig.java
@@ -2,16 +2,11 @@ package com.minorproject.library.e_Library.security;
 
 import com.minorproject.library.e_Library.security.filters.JwtAuthenticationFilter;
 import com.minorproject.library.e_Library.security.filters.JwtRequestFilter;
-import com.minorproject.library.e_Library.service.CustomOAuth2UserService;
-import com.minorproject.library.e_Library.service.MyUserDetailService;
 import com.minorproject.library.e_Library.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,8 +38,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), jwtUtil);
-//        jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");  // Setting the login URL for JWT authentication
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), jwtUtil);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");  // Setting the login URL for JWT authentication
 
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(authorize -> authorize
@@ -52,20 +47,11 @@ public class SecurityConfig {
                 .requestMatchers("/member/**").hasAnyRole("LIBRARIAN") // only librarian can access this endpoint
                 .requestMatchers("/auth/**", "/public/**").permitAll() // any user can access this endpoints (no need of authentication to access this endpoints)
                 .anyRequest().authenticated())
-            // Configuring OAuth2 login using GitHub as the provider
-            .oauth2Login(oauth2 -> oauth2
-                .loginPage("/oauth2/authorization/github")  // Custom login page for GitHub OAuth2
-                .defaultSuccessUrl("/", true) // Redirect users to "/" upon successful login
-                .userInfoEndpoint(userInfo -> userInfo.userService(this.customOAuth2UserService()))) // Custom logic to handle user info
-//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(this.jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build(); // Building the security filter chain
     }
 
-    @Bean
-    public CustomOAuth2UserService customOAuth2UserService() {
-        return new CustomOAuth2UserService();
-    }
 
 
     @Bean
